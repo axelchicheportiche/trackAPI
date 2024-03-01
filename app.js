9cb0d21e0c6236f3905d20c0d5498942b9e20f1b
axelchicheportiche <140517984+axelchicheportiche@users.noreply.github.com>
2024-03-01 18:30:33 +0100

    API call counter added

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
+//API Call
+let apiCalls = 0;
+
 module.exports = function (app) {
+
+    //Compteur d'appel API
+    app.use('/titre_aleatoire', (req, res, next) => {
+        apiCalls++;
+        next();
+    });
+
+
     app.get('/titre_aleatoire', (req, res) => {
         const filePath = path.join(__dirname, 'track.json');
         fs.readFile(filePath, 'utf8', (err, data) => {
@@ -25,5 +36,10 @@ module.exports = function (app) {
                 res.status(500).send('Erreur interne du serveur - Impossible de traiter le fichier JSON');
             }
         });
+    });
+
+    // Endpoint pour récupérer le nombre d'appels API
+    app.get('/statistiques', (req, res) => {
+        res.json({ apiCalls });
     });
 };
